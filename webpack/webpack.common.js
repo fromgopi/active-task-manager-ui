e1f465c05fd848019e751577f12aa1651cf088a7
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-08-06 22:10:36 +0530

    pushing some webpack items
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,4 +3,3 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const commonPaths = require('./path');
-
