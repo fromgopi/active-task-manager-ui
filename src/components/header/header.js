b4b047142a12495ec35370ffefc6f73fe48795e3
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-08-26 21:38:47 +0530

    adding more code for models
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 
 const Header = () => {
+    const [searchModalOpen, setSearchModalOpen] = useState(false);
     return(
-        <div className='header-2'>
-            <nav className='bg-white py-2 md:py-4'>
-                <div className='container px-4 mx-auto md:flex md:items-center'>
-                    <div className='flex justify-between items-center'>
-                        <a href='#' className='font-bold text-xl logo-text-color'>AI</a>
-                        <button 
-                        className = 'border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden'>
-                            <i className='fas fa-bars'>Home</i>
-                        </button>
-                    </div>
-                    <div className='hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0'>
-                        <a href="#" className="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600">Home</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <header className='sticky top-0 bg-white border-b border-y-slate-200 z-30'>
+            <div className='px-4'></div>
+            <div className='flex items-center'>
+                <svg className="w-4 h-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path className="fill-current text-slate-500" d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
+                    <path className="fill-current text-slate-400" d="M15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
+                </svg>
+            </div>
+        </header>
     )
 }
 
