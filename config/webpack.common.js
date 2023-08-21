c80d5c4dc724dc164410f961a8b51d18863e2f37
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-08-21 13:32:54 +0530

    Update webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,7 +3,7 @@ const paths = require("./paths");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");  
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extract css to files
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // extract css to files 
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer') // help tailwindcss to work 
 
